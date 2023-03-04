1c121fa306c57ac35e1c7ea434741d004ea894c1
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-03-04 23:22:37 +0100

    unit testing done

--- German.Core/Interfaces/ICourseService.cs
+++ German.Core/Interfaces/ICourseService.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using German.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace German.Core.Interfaces
 {
     public interface ICourseService
     {
+        Task<List<Course>> GetCoursesAsync();
+
     }
 }
--- German.Test/Services/CourseServiceTest.cs
+++ German.Test/Services/CourseServiceTest.cs
@@ -5,15 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using German.Application.Services;
 using German.Core.Interfaces;
+using German.Core.Entities;
 using Moq;
 using Xunit;
+using FluentAssertions;
 
 namespace German.Test.Services
 {
-    class CourseServiceTest : ICourseService
+   public class CourseServiceTest
     {
-        private readonly Mock<IAppDbContext> _appDbContextMock;
-        private readonly ICourseService _courseServiceMock;
+        private  Mock<IAppDbContext> _appDbContextMock;
+        private  ICourseService _courseServiceMock;
 
         public CourseServiceTest() {
             this._appDbContextMock= new Mock<IAppDbContext>();
@@ -21,7 +23,43 @@ namespace German.Test.Services
         }
 
         [Fact]
+        public async Task getCoursesList()
+        {
+            //arrange
+            List<Course> database = new List<Course>
+            {
+                new Course
+                {
+                    Id = 1,
+                    Name = "Course 1"
+                },
+                new Course
+                {
+                    Id = 2,
+                    Name = "Course 2"
+                }
+            };
 
+            this._appDbContextMock.Setup(db => 
+                db.SelectAllCoursesAsync())
+                .ReturnsAsync(database.ToList());
+
+            //act
+            List<Course> actualCourses = await _courseServiceMock.GetCoursesAsync();
+
+
+            //assert
+            //db should be cslled once.
+            //returns a list of courses
+
+            //actualCourses.Should().NotBeEmpty();
+            actualCourses.Should().BeEquivalentTo(database);
+            _appDbContextMock.Verify(db => db.SelectAllCoursesAsync(), Times.Once);
+            _appDbContextMock.VerifyNoOtherCalls();
+
+
+
+        }
 
 
     }
