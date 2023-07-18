1a108eba9a286f5a1910945dd635009d52886ece
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-07-18 07:08:02 +0100

    first

---
+++ recommender/backend/connect.php
@@ -0,0 +1,30 @@
+<?php 
+// connection params using PDO
+$host = 'localhost';
+$dbUser = "root";
+$dbPass = "";
+$database = "recommender";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false
+];
+
+
+
+//connection to database using PDO by creating a new PDO object '$conn*-'
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$database",$dbUser,$dbPass,$options);
+
+}catch(PDOException $e){
+    echo json_encode([
+        'message' => 'Connection to database failed: '. $e->getMessage()
+    ]) ;
+    exit();
+}
+
+//insert into db;
+
+
+?>
---
+++ recommender/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CDKS</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo">CDKS</div>
+            <div class="login_and_signup">
+                <a id="loginlink" href="./login.php">Login</a>
+                <a id = "sign_up_link"href="./sign_up.php">Sign up</a>
+            </div>
+        </div>
+        <div class="container_body">
+            <div class="left_half index">
+                <div id="left_half_bigtext">Put your</div>
+                <div id="left_half_bigtext">money to work</div>
+                <div id="left_half_smalltext">invest wisely. Manage risk.</div>
+                <div id="left_half_smalltext">Let's do the search for you.</div>
+                <form action="">
+                    <input type="email" placeholder="email">
+                    <input type="submit" value="Get started">
+                </form>
+            </div>
+            <div class="right_half">
+                <div class="img_container">
+                    <img width="500px" height="616px" src="./src/Vector.png" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
