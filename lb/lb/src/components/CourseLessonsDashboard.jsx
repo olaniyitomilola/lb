6c50ca20ac3116e873501df05890465453e3b844
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-08-19 21:51:52 +0100

    course lessons

---
+++ lb/lb/src/components/CourseLessonsDashboard.jsx
@@ -0,0 +1,28 @@
+export default function CourseLessonDashboard(){
+    return(
+        <div className="courseLessonDashboard">
+            <div className="courseBannerAndProgress">
+                <div className="courseBanner">
+
+                </div>
+                <div className="courseProgress">
+
+                </div>
+            </div>
+            <div className="courseContentAndCourseLessons">
+                <div className="courseContent">
+                    <div className="generalIntro">
+
+                    </div>
+                    <div className="courseAssessment">
+                        
+                    </div>
+
+                </div>
+                <div className="courseLessons">
+
+                </div>
+            </div>
+        </div>
+    )
+}
